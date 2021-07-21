c1810b8ad6b23aa86941d17e916b99313c55791b
Ivan <waizord@yandex.ru>
2021-07-21 04:32:52 +0300

    Fix: fixed color buttons

--- RSSchool_T8/ArtistView/PaleteView/PaletteViewController.m
+++ RSSchool_T8/ArtistView/PaleteView/PaletteViewController.m
@@ -26,37 +26,78 @@
 
 @property (nonatomic, readonly) UIColor *lightGreenSea;
 @property (nonatomic, readonly) UIColor *chillSky;
+@property (nonatomic, readonly) UIColor *red;
+@property (nonatomic, readonly) UIColor *blue;
+@property (nonatomic, readonly) UIColor *green;
+@property (nonatomic, readonly) UIColor *gray;
+@property (nonatomic, readonly) UIColor *purp;
+@property (nonatomic, readonly) UIColor *lightRed;
+@property (nonatomic, readonly) UIColor *orange;
+@property (nonatomic, readonly) UIColor *lightBlue;
+@property (nonatomic, readonly) UIColor *pink;
+@property (nonatomic, readonly) UIColor *darkBlue;
+@property (nonatomic, readonly) UIColor *darkGreen;
+@property (nonatomic, readonly) UIColor *brown;
 
-
+@property (nonatomic, strong) CAShapeLayer *maskColor;
 @property (nonatomic, strong) UIView *viewSap;
 @end
 
 @implementation PaletteViewController
 
-//MARK: Constants
+//MARK: Constant colors
 - (UIColor *)lightGreenSea {
     return [[UIColor alloc] initWithRed:0.13 green:0.692 blue:0.557 alpha:1];
 }
 - (UIColor *)chillSky {
     return [[UIColor alloc] initWithRed:0.0 green:0.7 blue:1.0 alpha:1];
 }
+- (UIColor *)red {
+    return [[UIColor alloc] initWithRed:0.886 green:0.106 blue:0.173 alpha:1];
+}
+- (UIColor *)blue {
+    return [[UIColor alloc] initWithRed:0.243 green:0.09 blue:0.8 alpha:1];
+}
+- (UIColor *)green {
+    return [[UIColor alloc] initWithRed:0.0 green:0.486 blue:0.216 alpha:1];
+}
+- (UIColor *)gray {
+    return [[UIColor alloc] initWithRed:0.502 green:0.502 blue:0.502 alpha:1];
+}
+- (UIColor *)purp {
+    return [[UIColor alloc] initWithRed:0.616 green:0.369 blue:0.918 alpha:1];
+}
+- (UIColor *)lightRed {
+    return [[UIColor alloc] initWithRed:1.0 green:0.478 blue:0.408 alpha:1];
+}
+- (UIColor *)orange {
+    return [[UIColor alloc] initWithRed:1.0 green:0.678 blue:0.329 alpha:1];
+}
+- (UIColor *)lightBlue {
+    return [[UIColor alloc] initWithRed:0.0 green:0.682 blue:0.929 alpha:1];
+}
+- (UIColor *)pink {
+    return [[UIColor alloc] initWithRed:1.0 green:0.476 blue:0.635 alpha:1];
+}
+- (UIColor *)darkBlue {
+    return [[UIColor alloc] initWithRed:0.0 green:0.18 blue:0.235 alpha:1];
+}
+- (UIColor *)darkGreen {
+    return [[UIColor alloc] initWithRed:0.055 green:0.216 blue:0.094 alpha:1];
+}
+- (UIColor *)brown {
+    return [[UIColor alloc] initWithRed:0.38 green:0.059 blue:0.063 alpha:1];
+}
 
-
-//-(UIButton *)saveButton {
-//
-//    return button;
-//}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self.view addSubview:self.saveButton];
-    [self.view addSubview:self.greenButton];
 }
 
 -(void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:true];
-    self.view.backgroundColor = UIColor.whiteColor;
+    
     [self viewStyle];
     [self buttonsStyle];
 }
@@ -88,26 +129,40 @@
     [self.saveButton addTarget:self action:@selector(saveButtonPressed:)
          forControlEvents:UIControlEventTouchUpInside];
     
-    CAShapeLayer *maskColor = [[CAShapeLayer alloc] init];
-    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(8, 8, 24.0, 24.0) cornerRadius:6.0];
-//    UIBezierPath *pat = UIBezierPath bezierPathWithArcCenter:<#(CGPoint)#> radius:<#(CGFloat)#> startAngle:<#(CGFloat)#> endAngle:<#(CGFloat)#> clockwise:<#(BOOL)#>
-    maskColor.path = path.CGPath;
-    maskColor.backgroundColor = UIColor.blueColor.CGColor;
     
-    self.redButton.backgroundColor = UIColor.whiteColor;
-    self.redButton.tag = 1;
-    self.redButton.layer.masksToBounds = NO;
-    self.redButton.layer.cornerRadius = 10;
-    self.redButton.layer.shadowColor = [UIColor.blackColor colorWithAlphaComponent:0.25].CGColor;
-    self.redButton.layer.shadowRadius = 2.0;
-    self.redButton.layer.shadowOffset = CGSizeZero;
-    self.redButton.layer.shadowOpacity = 1.0;
-    [self.redButton.layer addSublayer:maskColor];
-    
+    self.redButton = [self setStyleFor:self.redButton layer:self.maskColor color:self.red tag:1];
+    self.blueButton = [self setStyleFor:self.blueButton layer:self.maskColor color:self.blue tag:2];
+    self.greenButton = [self setStyleFor:self.greenButton layer:self.maskColor color:self.green tag:3];
+    self.grayButton = [self setStyleFor:self.grayButton layer:self.maskColor color:self.gray tag:4];
+    self.purpButton = [self setStyleFor:self.purpButton layer:self.maskColor color:self.purp tag:5];
+    self.lightRedButton = [self setStyleFor:self.lightRedButton layer:self.maskColor color:self.lightRed tag:6];
+    self.orangeButton = [self setStyleFor:self.orangeButton layer:self.maskColor color:self.orange tag:7];
+    self.lightBlueButton = [self setStyleFor:self.lightBlueButton layer:self.maskColor color:self.lightBlue tag:8];
+    self.pinkButton = [self setStyleFor:self.pinkButton layer:self.maskColor color:self.pink tag:9];
+    self.darkBlueButton = [self setStyleFor:self.darkBlueButton layer:self.maskColor color:self.darkBlue tag:10];
+    self.darkGreenButton = [self setStyleFor:self.darkGreenButton layer:self.maskColor color:self.darkGreen tag:11];
+    self.brownButton = [self setStyleFor:self.brownButton layer:self.maskColor color:self.brown tag:12];
 }
--(UIButton *) setButton: (CALayer *)layer : (UIColor *)color {
-    UIButton *button = [UIButton new];
+    //MARK: Custom style buttons and mask
+-(UIButton *) setStyleFor: (UIButton *)button layer:(CAShapeLayer*) layer color:(UIColor *)color tag:(NSInteger) tag {
+    button.backgroundColor = UIColor.whiteColor;
+    button.tag = tag;
+    button.layer.masksToBounds = NO;
+    button.layer.cornerRadius = 10;
+    button.layer.shadowColor = [UIColor.blackColor colorWithAlphaComponent:0.25].CGColor;
+    button.layer.shadowRadius = 2.0;
+    button.layer.shadowOffset = CGSizeZero;
+    button.layer.shadowOpacity = 1.0;
+    CALayer *layerMask = [self setCustomMask:color];
+    [button.layer addSublayer:layerMask];
     return button;
+}
+-(CAShapeLayer *) setCustomMask: (UIColor *) color {
+    CAShapeLayer *layer = [[CAShapeLayer alloc] init];
+    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(8, 8, 24.0, 24.0) cornerRadius:6.0];
+    layer.path = path.CGPath;
+    layer.fillColor = color.CGColor;
+    return layer;
 }
 //MARK: - Actions
 - (void)saveButtonPressed:(UIButton *)button {
@@ -116,6 +171,7 @@
 }
 - (IBAction)selectColor:(UIButton *)sender {
     NSLog(@"%lu", sender.tag);
+    
 }
 
 @end
